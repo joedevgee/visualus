5a86b12879210c4a72d126986c479f75d4be81f9
qliu <qliu@jcp.com>
2017-11-10 02:46:22 -0600

    Updated watchman to fix error

--- src/component/HeaderBar/HeaderBar.jsx
+++ src/component/HeaderBar/HeaderBar.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 
+/**
+ * The nav bar which appears on top of the website
+ * Or appbar
+ */
 const HeaderBar = () => (
-  <AppBar
-    title="Title"
-    iconClassNameRight="muidocs-icon-navigation-expand-more"
-  />
+  <div>
+    <AppBar
+      title="Title"
+      iconClassNameRight="muidocs-icon-navigation-expand-more"
+    />
+  </div>
 );
 
 export default HeaderBar;
---
+++ styleguide.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    components: 'src/component/**/*.jsx'
+}
