874df735ff274a66fc928694e816e7366023a41e
datageeeek <hutaotao511@gmail.com>
2018-01-02 20:57:14 -0600

    Updated test with async await

--- src/api/googlePlaces.test.js
+++ src/api/googlePlaces.test.js
@@ -3,15 +3,14 @@ import MockAdapter from 'axios-mock-adapter';
 import GooglePlaces from './googlePlaces';
 
 describe('Google Places API', () => {
-  it('can fetch', done => {
+  it('can fetch', async () => {
+    expect.assertions(1);
     var mock = new MockAdapter(axios);
     const url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json';
     mock.onGet(url).reply(200, {
       city: 'Boston'
     });
-    GooglePlaces.fetchPlaces().then(resp => {
-      expect(resp.city).toEqual('Boston');
-      done();
-    });
+    const data = await GooglePlaces.fetchPlaces();
+    expect(data.city).toEqual('Boston');
   });
 });
