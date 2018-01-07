eea365e2f91c6c067cdbb40e9f411cbbb327ffa1
datageeeek <hutaotao511@gmail.com>
2018-01-07 16:05:15 -0600

    Moved layout logic

--- src/wrapper/education/education.js
+++ src/wrapper/education/education.js
@@ -1,10 +1,18 @@
 // @flow
 import React from 'react';
+import { Layout } from 'antd';
 
 import './education.css';
 
 const Education = () => {
-  return <h1>This is education</h1>;
+  const { Content } = Layout;
+  return (
+    <Content style={{ padding: '8px' }}>
+      <div className="education">
+        <h2>This is education</h2>
+      </div>
+    </Content>
+  );
 };
 
 export default Education;
--- src/wrapper/home/home.js
+++ src/wrapper/home/home.js
@@ -1,9 +1,18 @@
 // @flow
 import React from 'react';
+import { Layout } from 'antd';
+
 import './home.css';
 
 const Home = () => {
-  return <h1>This is home</h1>;
+  const { Content } = Layout;
+  return (
+    <Content style={{ padding: '50px' }}>
+      <div className="home">
+        <h1>This is home</h1>
+      </div>
+    </Content>
+  );
 };
 
 export default Home;
