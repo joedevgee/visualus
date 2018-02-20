d77d015b2c9e4cb4c854b22e4950d4ec69c63a44
qliu <qliu@jcp.com>
2018-02-19 17:01:39 -0800

    Fixed test failure and test coverage

--- src/container/education/education.js
+++ src/container/education/education.js
@@ -2,22 +2,12 @@
 import { connect } from 'react-redux';
 import type { Connector } from 'react-redux';
 import type { State, Dispatch } from '../../type';
-import type { Id, UniversityList, University } from '../../type/university';
 
 import { beginFetchUniversity } from '../../action/fetchUniversity';
 import { setSelectUniversity } from '../../action/selectUniversity';
 
 import type { Props } from '../../wrapper/education/education';
 import Education from '../../wrapper/education/education';
-
-// TODO: create separate selector and add unit test for selectors
-const getSelectedUniversity = (list: UniversityList, selectedId?: Id) => {
-  if (selectedId) {
-    return list.find((u: University) => u.id === selectedId);
-  } else {
-    return null;
-  }
-};
 
 const mapStateToProps = (state: State) => {
   return {
